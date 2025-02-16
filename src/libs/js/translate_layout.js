a1d8631b1a3e31163e8b9ee1a8991d9163dff77c
Lucas Oliveira <153150990+LukasO20@users.noreply.github.com>
2025-02-15 23:06:14 -0300

    Added initial structure and implement more data of language json archives
    - More data to language json archives
    - Initial specifications to HTML elements to identify language by attributes

--- src/libs/js/translate_layout.js
+++ src/libs/js/translate_layout.js
@@ -1,5 +1,10 @@
 const translate = () => {
+    //OS ELEMENTOS DE ATRIBUTO lang serão agrupados em um array
+    //O array irá percorrer elementos de acordo com o index e efetuará a tradução
 
+    //com elementos de atributo lang juntos em um array, evitará qualquer tipo de erro caso novos elementos sejam adicionados na tela
+
+    
 }
 
 export { translate }
